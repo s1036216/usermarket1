5dff956d4cf1e0f26b3de25123d98d842d41d13f
cho <cho1036216@naver.com>
2017-08-22 10:05:58 +0900

    8-22
    .

---
+++ src/dml.sql
@@ -0,0 +1,120 @@
+
+create table item(
+	seq NUMBER,
+	title VARCHAR2(30),
+	writer VARCHAR2(30),
+	regdate date,
+	photo VARCHAR2(30),
+	content VARCHAR2(30)
+);
+
+select * from item;
+	<!-- seq ,title,	writer,regdate,photo,content  -->
+insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가나하');
+insert into item 
+		values (seq.nextval,'컴퓨터','조봉',sysdate,'defult.jsp','가나4하');
+		insert into item 
+		values (seq.nextval,'컴퓨터매','조기',sysdate,'defult.jsp','가나4하');
+		insert into item 
+		values (seq.nextval,'컴퓨터매','조',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조',sysdate,'defult.jsp','가나214하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉1',sysdate,'defult.jsp','가나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉2',sysdate,'defult.jsp','가나5하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기3',sysdate,'defult.jsp','가4나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기4',sysdate,'defult.jsp','가12나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉5기',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기6',sysdate,'defult.jsp','가나s하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉7기',sysdate,'defult.jsp','가나5하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기8',sysdate,'defult.jsp','가5나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉4기',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조9봉기',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉0기',sysdate,'defult.jsp','가67나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉11기',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기12',sysdate,'defult.jsp','가나s하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기13',sysdate,'defult.jsp','가5나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기14',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기15',sysdate,'defult.jsp','가22나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기16',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉15기',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기22',sysdate,'defult.jsp','가54나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉1기',sysdate,'defult.jsp','가4나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉2기',sysdate,'defult.jsp','가51나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉3기',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉4기',sysdate,'defult.jsp','가나5하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉5기',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉7기',sysdate,'defult.jsp','가1나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉9기',sysdate,'defult.jsp','가22나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉0기',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉24기',sysdate,'defult.jsp','가나5하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉12기',sysdate,'defult.jsp','가나4하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉15기',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉61기',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가나45하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉24기',sysdate,'defult.jsp','가나3하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉55기',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉2기',sysdate,'defult.jsp','가나12하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉3기',sysdate,'defult.jsp','가나41하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉5기',sysdate,'defult.jsp','가나24하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조1봉기',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조3봉기',sysdate,'defult.jsp','가나1하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조2봉기',sysdate,'defult.jsp','가5나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가4나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조1봉기',sysdate,'defult.jsp','가2나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가5나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가4나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가3나하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가나2하');
+		insert into item 
+		values (seq.nextval,'컴퓨터판매','조봉기',sysdate,'defult.jsp','가1나하');
+		
+		
